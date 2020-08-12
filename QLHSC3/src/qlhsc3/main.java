626fb62b5f5f467943386f316725731fe68a9784
ThanhTung468 <tttung468@gmail.com>
2020-08-12 19:35:01 +0700

    Abc

--- QLHSC3/src/qlhsc3/main.java
+++ QLHSC3/src/qlhsc3/main.java
@@ -5,19 +5,31 @@
  */
 package qlhsc3;
 
-import DAO.AccountDAO;
-import DAO.ConducttypeDAO;
-import DAO.EthnicDAO;
+
+import DAO.HRStaffDAO;
+import DAO.OfficeStaffDAO;
+import DAO.RoleDAO;
+import DAO.SchoolClassDAO;
+import DAO.SchoolYearDAO;
+import DAO.ScoreDAO;
 import DAO.StudentDAO;
+import DAO.SubjectDAO;
+import DAO.TeacherAssignmentDAO;
+import DAO.TeacherDAO;
 import connection.HibernateUtil;
 import java.util.List;
-import org.hibernate.engine.spi.ExecutableList;
-import org.hibernate.engine.spi.ExecutableList.Sorter;
-import pojos.Account;
-import pojos.Conducttype;
-import pojos.Ethnic;
+import pagination.PageRequest;
+import pojos.HRStaff;
+import pojos.OfficeStaff;
 import pojos.Role;
+import pojos.SchoolClass;
+import pojos.SchoolYear;
+import pojos.Score;
 import pojos.Student;
+import pojos.Subject;
+import pojos.Teacher;
+import pojos.TeacherAssignment;
+import sort.Sorter;
 
 /**
  *
@@ -29,8 +41,30 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-                
+        SubjectDAO dao = new SubjectDAO();
+        Subject filterModel;
+        Integer page = 1;
+	Integer perPage = 6;
+	Sorter sorter = new Sorter("subject.id", "desc");
+        String searchKey;       
+        
+	searchKey = null;
+	filterModel = null;
+//        searchKey = null;
+//        filterModel = dao.findOne(2);        
+//        searchKey = "english";
+//        filterModel = null;
+        
+        PageRequest<Subject> pageRequest = new PageRequest<>(page, perPage, sorter, searchKey, filterModel);
+        List<Subject> list = dao.find(pageRequest);
+        System.out.println("\nTest find :");
+        for (Subject pojo : list) {
+            System.out.println(pojo.getId() + " - " + pojo.getCode());
+        }
+        
+        System.out.println("\nTest count :");
+        System.out.println(dao.count(pageRequest));
+        
         
         
         //close sessionFactory
