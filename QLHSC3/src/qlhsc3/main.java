9dcca638b9232dc8272abbf572c34f3239be5c64
ThanhTung468 <tttung468@gmail.com>
2020-08-16 21:38:34 +0700

    Override find() va count() cho StudentOfClassDAO

--- QLHSC3/src/qlhsc3/main.java
+++ QLHSC3/src/qlhsc3/main.java
@@ -19,6 +19,8 @@ import DAO.SubjectDAO;
 import DAO.TeacherAssignmentDAO;
 import DAO.TeacherDAO;
 import BUS.AccountBUS;
+import BUS.StudentOfClassBUS;
+import BUS.SubjectBUS;
 import connection.HibernateUtil;
 import java.util.List;
 import pagination.PageRequest;
@@ -32,6 +34,7 @@ import pojos.SchoolClass;
 import pojos.SchoolYear;
 import pojos.Score;
 import pojos.Student;
+import pojos.StudentOfClass;
 import pojos.Subject;
 import pojos.Teacher;
 import pojos.TeacherAssignment;
@@ -47,34 +50,34 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        SubjectDAO dao = new SubjectDAO();
-//        Subject filterModel;
-//        Integer page = 1;
-//	Integer perPage = 6;
-//	Sorter sorter = new Sorter("subject.id", "desc");
-//        String searchKey;       
-//        
-//	searchKey = null;
-//	filterModel = null;
-////        searchKey = null;
-////        filterModel = dao.findOne(2);        
-////        searchKey = "english";
-////        filterModel = null;
-//        
-//        PageRequest<Subject> pageRequest = new PageRequest<>(page, perPage, sorter, searchKey, filterModel);
-//        List<Subject> list = dao.find(pageRequest);
-//        System.out.println("\nTest find :");
-//        for (Subject pojo : list) {
-//            System.out.println(pojo.getId() + " - " + pojo.getCode());
-//        }
-//        
-//        System.out.println("\nTest count :");
-//        System.out.println(dao.count(pageRequest));
+        StudentOfClassBUS bus = new StudentOfClassBUS();
+        StudentOfClass filterModel;
+        Integer page = 1;
+	Integer perPage = 6;
+	Sorter sorter = new Sorter("studentOfClass.id", "desc");
+        String searchKey;       
         
-        AccountBUS bus = new AccountBUS();
-        for (Account a : bus.findAll()) {
-            System.out.println(a.getId() + "  " + a.getUsername());
+	searchKey = null;
+	filterModel = null;
+//        searchKey = null;
+//        filterModel = bus.findOne(3);        
+//        searchKey = "7";
+//        filterModel = null;
+        
+        PageRequest<StudentOfClass> pageRequest = new PageRequest<>(page, perPage, sorter, searchKey, filterModel);
+        List<StudentOfClass> list = bus.find(pageRequest);
+        System.out.println("\nTest find :");
+        for (StudentOfClass pojo : list) {
+            System.out.println(pojo.getId() + " - " + pojo.getStudent().getCode());
         }
+        
+        System.out.println("\nTest count :");
+        System.out.println(bus.count(pageRequest));
+        
+//        AccountBUS bus = new AccountBUS();
+//        for (Account a : bus.findAll()) {
+//            System.out.println(a.getId() + "  " + a.getUsername());
+//        }
         
         
         //close sessionFactory
